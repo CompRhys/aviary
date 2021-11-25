0f6c92d5ac850956a2700da99887c1fdb5e6a0e9
Rhys Goodall <reag2@cam.ac.uk>
2021-11-25 14:23:03 +0000

    refactor: clean tests

--- tests/test_wyckoff_ops.py
+++ tests/test_wyckoff_ops.py
@@ -16,22 +16,19 @@ def test_get_aflow_label_spglib():
         s = s.read()
         struct = Structure.from_str(s, fmt="cif")
 
-    aflow = get_aflow_label_spglib(struct)
-    assert aflow == "ABC6D2_mC40_15_e_e_3f_f:Ca-Fe-O-Si"
+    assert get_aflow_label_spglib(struct) == "ABC6D2_mC40_15_e_e_3f_f:Ca-Fe-O-Si"
 
 
 def test_count_wyks():
     """Count the number of wyckoff positions in esseneite
     """
-    count = count_wyks("ABC6D2_mC40_15_e_e_3f_f:Ca-Fe-O-Si")
-    assert count == 6
+    assert count_wyks("ABC6D2_mC40_15_e_e_3f_f:Ca-Fe-O-Si") == 6
 
 
 def test_count_params():
     """Count the number of coarse-grained parameters in esseneite
     """
-    count = count_params("ABC6D2_mC40_15_e_e_3f_f:Ca-Fe-O-Si")
-    assert count == 18
+    assert count_params("ABC6D2_mC40_15_e_e_3f_f:Ca-Fe-O-Si") == 18
 
 
 if __name__ == "__main__":
