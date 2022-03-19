361801aade9d2b187c83bcb3d332b125f35cc839
Rhys Goodall <reag2@cam.ac.uk>
2022-03-19 14:35:24 -0700

    doc: finish losses.py

--- aviary/losses.py
+++ aviary/losses.py
@@ -3,34 +3,40 @@ import torch
 from torch import Tensor
 
 
-def RobustL1Loss(output: Tensor, log_std: Tensor, target: Tensor) -> Tensor:
+def RobustL1Loss(pred_mean: Tensor, pred_log_std: Tensor, target: Tensor) -> Tensor:
     """Robust L1 loss using a Lorentzian prior. Allows for estimation
     of an aleatoric uncertainty.
 
     Args:
-        output (Tensor): _description_
-        log_std (Tensor): _description_
-        target (Tensor): _description_
+        pred_mean (Tensor): Tensor of predicted means
+        pred_log_std (Tensor): Tensor of predicted log standard deviations
+        target (Tensor): Tensor of target values
 
     Returns:
-        Tensor: _description_
+        Tensor: Evaluated robust L1 loss
     """
-    loss = np.sqrt(2.0) * torch.abs(output - target) * torch.exp(-log_std) + log_std
+    loss = (
+        np.sqrt(2.0) * torch.abs(pred_mean - target) * torch.exp(-pred_log_std)
+        + pred_log_std
+    )
     return torch.mean(loss)
 
 
-def RobustL2Loss(output: Tensor, log_std: Tensor, target: Tensor) -> Tensor:
+def RobustL2Loss(pred_mean: Tensor, pred_log_std: Tensor, target: Tensor) -> Tensor:
     """Robust L2 loss using a Gaussian prior. Allows for estimation
     of an aleatoric uncertainty.
 
     Args:
-        output (Tensor): _description_
-        log_std (Tensor): _description_
-        target (Tensor): _description_
+        pred_mean (Tensor): Tensor of predicted means
+        pred_log_std (Tensor): Tensor of predicted log standard deviations
+        target (Tensor): Tensor of target values
 
     Returns:
-        Tensor: _description_
+        Tensor: Evaluated robust L2 loss
     """
     # NOTE can we scale log_std by something sensible to improve the OOD behaviour?
-    loss = 0.5 * torch.pow(output - target, 2.0) * torch.exp(-2.0 * log_std) + log_std
+    loss = (
+        0.5 * torch.pow(pred_mean - target, 2.0) * torch.exp(-2.0 * pred_log_std)
+        + pred_log_std
+    )
     return torch.mean(loss)
