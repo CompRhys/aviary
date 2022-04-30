6f81d754fb8957dab1d1c857d114e13925c9ac71
Janosh Riebesell <janosh.riebesell@gmail.com>
2022-04-30 10:03:03 +0100

    drop parse_aflow() from aviary/wrenformer/data.py, import from aviary/wren/data.py
    rename cry_ids -> material_ids

--- aviary/wrenformer/data.py
+++ aviary/wrenformer/data.py
@@ -2,9 +2,7 @@ from __future__ import annotations
 
 import functools
 import json
-import re
-from itertools import groupby
-from os.path import abspath, dirname, exists, join
+from os.path import dirname, exists, join
 from typing import Sequence
 
 import numpy as np
@@ -13,7 +11,7 @@ import torch
 from torch import LongTensor, Tensor, nn
 from torch.utils.data import Dataset
 
-from aviary.wren.utils import mult_dict, relab_dict
+from aviary.wren.data import parse_aflow
 
 
 class WyckoffData(Dataset):
@@ -37,7 +35,7 @@ class WyckoffData(Dataset):
             element_embedding (str, optional): One of "matscholar200", "cgcnn92", "megnet16", "onehot112" or
                 path to a file with custom element embeddings. Defaults to "matscholar200".
             symmetry_embedding (str): Symmetry embedding. One of "bra-alg-off" (default) or "spg-alg-off".
-            input_col (str, optional): df columns to be used for featurisation.
+            input_col (str, optional): Name of df column containing Aflow-style Wyckoff strings.
                 Defaults to "wyckoff".
             id_cols (list, optional): df columns for distinguishing data points. Will be
                 copied over into the model's output CSV. Defaults to ["material_id", "composition"].
@@ -103,7 +101,7 @@ class WyckoffData(Dataset):
         """
         df_idx = self.df.iloc[idx]
         swyks = df_idx[self.inputs]
-        cry_ids = df_idx[self.identifiers].to_list()
+        material_ids = df_idx[self.identifiers].to_list()
 
         spg_no, weights, elements, aug_wyks = parse_aflow(swyks)
         weights = np.atleast_2d(weights).T / np.sum(weights)
@@ -111,7 +109,9 @@ class WyckoffData(Dataset):
         try:
             element_features = np.vstack([self.elem_features[el] for el in elements])
         except AssertionError:
-            print(f"Failed to process elements in {cry_ids[0]}: {cry_ids[1]}-{swyks}")
+            print(
+                f"Failed to process elements in {material_ids[0]}: {material_ids[1]}-{swyks}"
+            )
             raise
 
         try:
@@ -120,7 +120,7 @@ class WyckoffData(Dataset):
             )
         except AssertionError:
             print(
-                f"Failed to process Wyckoff positions in {cry_ids[0]}: {cry_ids[1]}-{swyks}"
+                f"Failed to process Wyckoff positions in {material_ids[0]}: {material_ids[1]}-{swyks}"
             )
             raise
 
@@ -136,11 +136,11 @@ class WyckoffData(Dataset):
         targets = []
         for name in self.task_dict:
             if self.task_dict[name] == "regression":
-                targets.append(Tensor([float(self.df.iloc[idx][name])]))
+                targets.append(Tensor([float(self.df[name].iloc[idx])]))
             elif self.task_dict[name] == "classification":
-                targets.append(LongTensor([int(self.df.iloc[idx][name])]))
+                targets.append(LongTensor([int(self.df[name].iloc[idx])]))
 
-        return combined_features, targets, *cry_ids
+        return combined_features, targets, *material_ids
 
 
 def collate_batch(
@@ -150,14 +150,14 @@ def collate_batch(
     crystal properties.
 
     Args:
-        dataset_list ([tuple]): list of tuples for each data point.
+        dataset_list ([tuple]): list of 3-tuples for each data point.
             (elem_fea, nbr_fea, nbr_idx, target)
 
             elem_fea (Tensor): Node features from atom type and Wyckoff letter
             nbr_fea (Tensor): _description_
             nbr_idx (LongTensor):
-            target (Tensor):
-            cif_id: str or int
+            targets (Tensor | LongTensor): Ground truth floats for regression or class labels.
+            material_ids: str or int
 
     Returns:
         tuple[
@@ -167,60 +167,13 @@ def collate_batch(
             *tuple[str | int]]: Identifiers like material_id, composition
         ]
     """
-
-    features, targets, *cry_ids = zip(*dataset_list)
+    features, targets, *material_ids = zip(*dataset_list)
     padded_features = nn.utils.rnn.pad_sequence(features, batch_first=True)
     mask = padded_features == 0
+    # padded_features.shape = (batch_size, max_seq_len, n_features), so we mask sequence items that
+    # are all non-zero across feature dimension
     mask = torch.all(mask, dim=2)
 
     targets = torch.tensor(targets).T
 
-    return ((padded_features, mask), targets, *zip(*cry_ids))
-
-
-def parse_aflow(
-    aflow_label: str,
-) -> tuple[str, list[float], list[str], list[tuple[str, ...]]]:
-    """Parse the Wren AFLOW-like Wyckoff encoding.
-
-    Args:
-        aflow_label (str): AFLOW-style prototype string with appended chemical system
-
-    Returns:
-        tuple[str, list[int], list[str], list[str]]: spg_no, mult_list, ele_list, aug_wyks
-    """
-    proto, chemsys = aflow_label.split(":")
-    elems = chemsys.split("-")
-    _, _, spg_no, *wyks = proto.split("_")
-
-    mult_list = []
-    ele_list = []
-    wyk_list = []
-
-    subst = r"1\g<1>"
-    for el, wyk in zip(elems, wyks):
-
-        # Put 1's in front of all Wyckoff letters not preceded by numbers
-        wyk = re.sub(r"((?<![0-9])[A-z])", subst, wyk)
-
-        # Separate out pairs of Wyckoff letters and their number of occurrences
-        sep_n_wyks = ["".join(g) for _, g in groupby(wyk, str.isalpha)]
-
-        for n, l in zip(sep_n_wyks[0::2], sep_n_wyks[1::2]):
-            m = int(n)
-            ele_list.extend([el] * m)
-            wyk_list.extend([l] * m)
-            mult_list.extend([float(mult_dict[spg_no][l])] * m)
-
-    # NOTE This on-the-fly augmentation of equivalent Wyckoff sets is potentially a source of high
-    # memory use. Can be turned off by commenting out the for loop and returning [wyk_list] instead
-    # of aug_wyks. Wren should be able to learn anyway.
-    aug_wyks = []
-    for trans in relab_dict[spg_no]:
-        # Apply translation dictionary of allowed relabelling operations in spacegroup
-        t = str.maketrans(trans)
-        aug_wyks.append(tuple(",".join(wyk_list).translate(t).split(",")))
-
-    aug_wyks = list(set(aug_wyks))
-
-    return spg_no, mult_list, ele_list, aug_wyks
+    return ((padded_features, mask), targets, *zip(*material_ids))
