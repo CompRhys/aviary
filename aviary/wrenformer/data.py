fix Wrenformer not averaging correctly over equivalent Wyckoff labellings
equivalent sets where previously stacked in the sequence dim (=1), they are now restacked into the batch dim (=0) in collate_batch() and averaged over after self-attention in Wrenformer.forward()

aviary/wrenformer/data.py
@@ -26,13 +26,29 @@ def collate_batch(
     Returns:
         tuple: Tuple of padded features and mask, targets and ids.
     """
-    padded_features = nn.utils.rnn.pad_sequence(tuple(features), batch_first=True)
-    # padded_features.shape = (batch_size, max_seq_len, n_features), so we mask sequence items that
-    # are all zero across feature dimension
+    if features[0].ndim == 3:
+        # wrenformer features are 3d with shape (n_equiv_wyksets [ragged],
+        # n_wyckoff_sites_per_set [ragged], n_features [uniform])
+        # we unpack the 1st dim into the batch dim and later take the mean of the
+        # transformer-encoded embeddings of equivalent sets
+        equivalence_counts = [len(tensor) for tensor in features]
+        # unpack 3d embedding tensors along dim=0 and restack equivalent Wyckoff sets
+        # along batch dim before padding
+        restacked = tuple(aug for emb in features for aug in emb)
+    else:
+        restacked = features  # for roostformer we do nothing
+
+    padded_features = nn.utils.rnn.pad_sequence(restacked, batch_first=True)
+
+    # padded_features.shape = (batch_size * mean_n_equiv_wyksets, max_seq_len, n_features),
+    # so we mask sequence items that are all zero across feature dimension
     mask = (padded_features == 0).all(dim=2)
 
     # insert outer dimension corresponding to different multi-tasking objectives
     targets = targets[None, ...]
+
+    if features[0].ndim == 3:
+        return (padded_features, mask, equivalence_counts), targets, ids
 
     return (padded_features, mask), targets, ids
 
@@ -45,45 +61,44 @@ with open(f"{PKG_DIR}/embeddings/element/matscholar200.json") as file:
 
 def wyckoff_embedding_from_aflow_str(wyckoff_str: str) -> Tensor:
     """Concatenate matscholar element and Wyckoff set embeddings while handling
-    augmentation from equivalent Wyckoff sets.
+    augmentation of equivalent Wyckoff sets.
 
     Args:
         wyckoff_str (str): Aflow-style Wyckoff string.
 
     Returns:
-        Tensor: Shape (n_augmentations, n_features).
+        Tensor: Shape (n_equiv_wyksets, n_wyckoff_sites, n_features).
     """
     spg_num, elem_weights, elements, augmented_wyckoffs = parse_aflow_wyckoff_str(
         wyckoff_str
     )
 
-    elem_weights = np.atleast_2d(elem_weights).T / np.sum(elem_weights)
-
-    element_features = np.vstack([elem_features[el] for el in elements])
-
-    symmetry_features = np.vstack(
-        [sym_features[spg_num][wyk] for wyks in augmented_wyckoffs for wyk in wyks]
+    symmetry_features = torch.tensor(
+        [
+            [sym_features[spg_num][wyk] for wyk in equivalent_wyckoff_set]
+            for equivalent_wyckoff_set in augmented_wyckoffs
+        ]
     )
 
     n_augments = len(augmented_wyckoffs)  # number of equivalent Wyckoff sets
-    # convert all data to tensors
-    element_ratios = torch.tensor(elem_weights).repeat(n_augments, 1)
-    element_features = torch.tensor(element_features).repeat(n_augments, 1)
-    symmetry_features = torch.tensor(symmetry_features)
+    element_features = torch.tensor([elem_features[el] for el in elements])
+    element_features = element_features[None, ...].repeat(n_augments, 1, 1)
+
+    element_ratios = torch.tensor(elem_weights)[None, :, None] / sum(elem_weights)
+    element_ratios = element_ratios.repeat(n_augments, 1, 1)
 
     combined_features = torch.cat(
-        [element_ratios, element_features, symmetry_features], dim=1
+        [element_ratios, element_features, symmetry_features], dim=-1
     ).float()
 
     return combined_features
 
 
 def get_composition_embedding(formula: str) -> Tensor:
-    """Concatenate matscholar element and Wyckoff set embeddings while handling
-    augmentation from equivalent Wyckoff sets.
+    """Concatenate matscholar element embeddings with element ratios in compostion.
 
     Args:
-        wyckoff_str (str): Aflow-style Wyckoff string.
+        formula (str): Composition string.
 
     Returns:
         Tensor: Shape (n_elements, n_features). Usually (2-6, 200).

aviary/wrenformer/model.py
@@ -81,7 +81,9 @@ class Wrenformer(BaseModelClass):
             ResidualNetwork(out_hidden[0], n, out_hidden[1:]) for n in n_targets
         )
 
-    def forward(self, features: Tensor, mask: BoolTensor) -> tuple[Tensor, ...]:  # type: ignore
+    def forward(  # type: ignore
+        self, features: Tensor, mask: BoolTensor, *args
+    ) -> tuple[Tensor, ...]:
         """Forward pass through the Wrenformer.
 
         Args:
@@ -91,12 +93,25 @@ class Wrenformer(BaseModelClass):
         Returns:
             tuple[Tensor, ...]: Predictions for each batch of multitask targets.
         """
+        # project input embedding onto d_model dimensions
         features = self.resize_embedding(features)
-        embedding = self.transformer_encoder(features, src_key_padding_mask=mask)
+        # run self-attention
+        embeddings = self.transformer_encoder(features, src_key_padding_mask=mask)
+
+        if len(args) == 1:
+            # running as wrenformer, not roostformer
+            equivalence_counts: list[int] = args[0]
+            # average over equivalent Wyckoff sets in a given material (brings len of dim 0
+            # back to batch_size)
+            equiv_embeddings = embeddings.split(equivalence_counts, dim=0)
+            augmented_embeddings = [tensor.mean(dim=0) for tensor in equiv_embeddings]
+            embeddings = torch.stack(augmented_embeddings)
+            # do the same for mask
+            mask = torch.stack([t[0] for t in mask.split(equivalence_counts, dim=0)])
 
         # aggregate all node representations into a single vector Wyckoff embedding
         # careful to ignore padded values when taking the mean
-        embedding_masked = embedding * ~mask[..., None]
+        embedding_masked = embeddings * ~mask[..., None]
         seq_lens = torch.sum(~mask, dim=1, keepdim=True)
         aggregated_embedding = torch.sum(embedding_masked, dim=1) / seq_lens
 