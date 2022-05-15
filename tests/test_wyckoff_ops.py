393f48339c3b8b829c2f38ff23070d37df18eb1f
Janosh Riebesell <janosh.riebesell@gmail.com>
2022-05-15 16:07:07 +0100

    refactor test_get_isopointal_proto() to use @pytest.mark.parametrize

--- tests/test_wyckoff_ops.py
+++ tests/test_wyckoff_ops.py
@@ -11,7 +11,7 @@ from aviary.wren.utils import (
 
 
 def test_get_aflow_label_spglib(tests_dir):
-    """Check that spglib gives correct aflow input for esseneite"""
+    """Check that spglib gives correct Aflow label for esseneite"""
     struct = Structure.from_file(f"{tests_dir}/data/ABC6D2_mC40_15_e_e_3f_f.cif")
 
     assert get_aflow_label_spglib(struct) == "ABC6D2_mC40_15_e_e_3f_f:Ca-Fe-O-Si"
@@ -27,24 +27,20 @@ def test_count_params():
     assert count_params("ABC6D2_mC40_15_e_e_3f_f:Ca-Fe-O-Si") == 18
 
 
-def test_get_isopointal_proto():
+@pytest.mark.parametrize(
+    "aflow_label, expected",
+    [
+        ("ABC6D2_mC40_15_e_e_3f_f:Ca-Fe-O-Si", "ABC2D6_mC40_15_e_e_f_3f"),
+        ("ABC6D2_mC40_15_e_a_3f_f:Ca-Fe-O-Si", "ABC2D6_mC40_15_a_e_f_3f"),
+        # failure of this case means doesn't single element materials
+        ("A_tI8_141_ea:Ca", "A_tI8_141_ae"),  # not ea is non-canonical
+        # failure of this case means not reordering elements based on int but first digit
+        ("A4BC20D2_oC108_41_2b_a_10b_b:B-Ca-H-N", "AB2C4D20_oC108_41_a_b_2b_10b"),
+    ],
+)
+def test_get_isopointal_proto(aflow_label, expected):
     """Get a recanonicalised prototype string without chemical system"""
-    assert (
-        get_isopointal_proto_from_aflow("ABC6D2_mC40_15_e_e_3f_f:Ca-Fe-O-Si")
-        == "ABC2D6_mC40_15_e_e_f_3f"
-    )
-    assert (
-        get_isopointal_proto_from_aflow("ABC6D2_mC40_15_e_a_3f_f:Ca-Fe-O-Si")
-        == "ABC2D6_mC40_15_a_e_f_3f"
-    )
-    assert (
-        get_isopointal_proto_from_aflow("A_tI8_141_ea:Ca")
-        == "A_tI8_141_ae"  # not ea is non-canonical
-    ), "failed to handle single element materials"
-    assert (
-        get_isopointal_proto_from_aflow("A4BC20D2_oC108_41_2b_a_10b_b:B-Ca-H-N")
-        == "AB2C4D20_oC108_41_a_b_2b_10b"
-    ), "failed to reorder elements based on int not first digit"
+    assert get_isopointal_proto_from_aflow(aflow_label) == expected
 
 
 @pytest.mark.parametrize(
