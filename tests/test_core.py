de2c24fa0adb55f0b94576f9eed51bbbc1b74fc4
Janosh Riebesell <janosh.riebesell@gmail.com>
2022-05-22 21:34:02 +0100

    add tests for masked_mean() and masked_std() in tests/test_core.py

--- tests/test_core.py
+++ tests/test_core.py
@@ -1,6 +1,8 @@
 import numpy as np
+import pytest
+import torch
 
-from aviary.core import np_one_hot, np_softmax
+from aviary.core import masked_mean, masked_std, np_one_hot, np_softmax
 
 
 def test_np_one_hot():
@@ -22,3 +24,44 @@ def test_np_softmax():
             # test numbers in softmaxed dimension all sum to 1
             out = np_softmax(xi, axis=axis)
             assert np.allclose(out.sum(axis=axis), 1)
+
+
+def test_masked_mean():
+    # test 1d tensor
+    x1 = torch.arange(5)
+    mask1 = torch.tensor([0, 1, 0, 1, 0]).bool()
+    assert masked_mean(x1, mask1) == 2
+
+    assert masked_mean(x1, mask1) == sum(x1 * mask1) / sum(mask1)
+
+    # test 2d tensor
+    x2 = torch.tensor([[1, 2, 3], [4, 5, 6]]).float()
+    mask2 = torch.tensor([[1, 1, 0], [0, 0, 1]]).bool()
+
+    assert masked_mean(x2, mask2) == pytest.approx([1, 2, 6])
+    assert masked_mean(x2, mask2, dim=1) == pytest.approx([1.5, 6])
+
+
+def test_masked_std():
+    # test 1d tensor
+    x1 = torch.arange(5)
+    mask1 = torch.tensor([0, 1, 0, 1, 0]).bool()
+    assert masked_std(x1, mask1) == 1
+
+    # test 2d tensor
+    x2 = torch.tensor([[1, 1, 1], [2, 2, 4]]).float()
+    mask2 = torch.tensor([[0, 1, 0], [1, 0, 1]]).bool()
+    assert masked_std(x2, mask2, dim=0) == pytest.approx([0, 0, 0], abs=1e-4)
+    assert masked_std(x2, mask2, dim=1) == pytest.approx([0, 1], abs=1e-4)
+
+    # test against explicit calculation
+    rand_floats = torch.rand(3, 4, 5)
+    rand_masks = torch.randint(0, 2, (3, 4, 5)).bool()
+    for xi, mask in zip(rand_floats, rand_masks):
+        for dim in (0, 1):
+            out = masked_std(xi, mask, dim=dim)
+            xi_nan = torch.where(mask, xi, torch.nan)
+            mean = xi_nan.nanmean(dim=dim)
+            std = (xi_nan - mean.unsqueeze(dim=dim)).pow(2).nanmean(dim=dim).sqrt()
+
+            assert out == pytest.approx(std, abs=1e-4, nan_ok=True)
