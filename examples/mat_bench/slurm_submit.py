9daa82a097618ccbb5624626bb232488f168b772
Janosh Riebesell <janosh.riebesell@gmail.com>
2022-06-15 13:35:12 +0100

    torch.save checkpoint swa_model if using SWA
    record number of trainable params in wandb config

--- examples/mat_bench/slurm_submit.py
+++ examples/mat_bench/slurm_submit.py
@@ -38,7 +38,7 @@ job_id = os.environ["SLURM_JOB_ID"]
 print(f"{{job_id=}}")
 print("{model_name=}")
 
-job_array_id = int(os.environ["SLURM_ARRAY_TASK_ID"])
+job_array_id = int(os.environ.get("SLURM_ARRAY_TASK_ID"))
 print(f"{{job_array_id=}}")
 
 dataset_name, fold = list(product({datasets}, {folds}))[job_array_id]
