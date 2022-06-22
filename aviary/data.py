f44c1ad4eb3bebce95796665987911189a30be64
Janosh Riebesell <janosh.riebesell@gmail.com>
2022-06-22 12:05:24 +0100

    add function df_to_in_mem_dataloader() to extract train/test_loader creation from run_wrenformer()

--- aviary/data.py
+++ aviary/data.py
@@ -15,15 +15,17 @@ class InMemoryDataLoader:
 
     Args:
         *tensors: List of arrays or tensors. Must all have the same length in dimension 0.
-        batch_size (int, optional): Defaults to 32.
+        batch_size (int, optional): Usually 64, 128 or 256. Can be larger for test set loaders
+            to speedup inference. Defaults to 64.
         shuffle (bool, optional): If True, shuffle the data *in-place* whenever an
             iterator is created from this object. Defaults to False.
         collate_fn (Callable, optional): Should accept variadic list of tensors and
             output a minibatch of data ready for model consumption. Defaults to tuple().
     """
 
-    tensors: list[Tensor]
-    batch_size: int = 32
+    # each item must be indexable (usually torch.tensor, np.array or pd.Series)
+    tensors: list[Tensor | np.ndarray]
+    batch_size: int = 64
     shuffle: bool = False
     collate_fn: Callable = tuple
 
