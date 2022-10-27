7f325f488e2d22a5d8db86762ca87994d605f846
Janosh Riebesell <janosh.riebesell@gmail.com>
2022-10-27 12:30:28 -0700

    refactor examples/wrenformer/{mp_wbm,mat_bench}/slurm_submit.py to use slurm_submit_python()

--- examples/wrenformer/mat_bench/slurm_submit.py
+++ examples/wrenformer/mat_bench/slurm_submit.py
@@ -1,23 +1,29 @@
 # %%
 import os
-import subprocess
 from datetime import datetime
+from itertools import product
 
 from matbench.metadata import mbv01_metadata
+from matbench_discovery.slurm import slurm_submit_python
 
+from aviary import ROOT
 from examples.wrenformer.mat_bench import DATA_PATHS
+from examples.wrenformer.mat_bench.train import train_wrenformer_on_matbench
+
+"""
+Train a Wrenformer ensemble of size n_folds on collection of Matbench datasets.
+"""
 
 __author__ = "Janosh Riebesell, Rokas Elijosius"
 __date__ = "2022-04-25"
 
 
-# %% write Python submission file and sbatch it
+# %%
 epochs = 300
-n_attn_layers = 3
-embedding_aggregations = ("mean", "std", "min", "max")
+target_col = "e_form"
+run_name = f"wrenformer-robust-{epochs=}-{target_col}"
 folds = list(range(5))
-checkpoint = None  # None | 'local' | 'wandb'
-lr = 1e-4
+today = f"{datetime.now():%Y-%m-%d}"
 model_name = f"wrenformer-robust-{epochs=}"
 
 if "roost" in model_name.lower():
@@ -27,63 +33,55 @@ else:
     # deploy Wren on structure tasks only
     datasets = [k for k, v in mbv01_metadata.items() if v.input_type == "structure"]
 
-log_dir = f"{os.path.dirname(__file__)}/job-logs"
-os.makedirs(log_dir, exist_ok=True)
-timestamp = f"{datetime.now():%Y-%m-%d@%H-%M-%S}"
+log_dir = f"{os.path.dirname(__file__)}/job-logs/{today}-{run_name}"
 
-python_script = f"""import os
-from datetime import datetime
-from itertools import product
-
-from examples.wrenformer.mat_bench.train_wrenformer import train_wrenformer_on_matbench
-
-print(f"Job started running {{datetime.now():%Y-%m-%d@%H-%M}}")
-print("{model_name=}")
-
-dataset_name, fold = list(product({datasets}, {folds}))[slurm_array_task_id]
-print(f"{{dataset_name=}}\\n{{fold=}}")
-
-train_wrenformer_on_matbench(
-    {model_name=},
-    dataset_name=dataset_name,
-    {timestamp=},
-    fold=fold,
-    {epochs=},
-    {n_attn_layers=},
-    {checkpoint=},
-    learning_rate={lr},
-    {embedding_aggregations=},
+slurm_submit_python(
+    job_name=run_name,
+    partition="ampere",
+    account="LEE-SL3-GPU",
+    time="8:0:0",
+    array=f"0-{len(datasets) * len(folds) - 1}",
+    log_dir=log_dir,
+    slurm_flags=("--nodes 1", "--gpus-per-node 1"),
+    # prepend into sbatch script to source module command and load default env
+    # for Ampere GPU partition before actual job command
+    pre_cmd=". /etc/profile.d/modules.sh; module load rhel8/default-amp;",
 )
-"""
-
-
-submit_script = f"{log_dir}/{timestamp}-{model_name}.py"
-
-# prepend into sbatch script to source module command and load default env
-# for Ampere GPU partition before actual job command
-slurm_setup = ". /etc/profile.d/modules.sh; module load rhel8/default-amp;"
 
 
 # %%
-slurm_cmd = f"""sbatch
-    --partition ampere
-    --account LEE-SL3-GPU
-    --time 8:0:0
-    --nodes 1
-    --gpus-per-node 1
-    --chdir {log_dir}
-    --array 0-{len(datasets) * len(folds) - 1}
-    --out {timestamp}-{model_name}-%A-%a.log
-    --job-name {model_name}
-    --wrap '{slurm_setup} python {submit_script}'
-"""
+n_attn_layers = 3
+embedding_aggregations = ("mean",)
+optimizer = "AdamW"
+learning_rate = 3e-4
+df_or_path = f"{ROOT}/datasets/2022-06-09-mp+wbm.json.gz"
+checkpoint = "wandb"  # None | 'local' | 'wandb'
+batch_size = 128
+swa_start = None
 
-header = f'"""\nSlurm submission command:\n{slurm_cmd}"""'
+slurm_job_id = os.environ["SLURM_JOB_ID"]
+slurm_array_task_id = int(os.environ.get("SLURM_ARRAY_TASK_ID", 0))
 
-with open(submit_script, "w") as file:
-    file.write(f"{header}\n\n{python_script}")
+print(f"Job started running {datetime.now():%Y-%m-%d@%H-%M}")
+print(f"{slurm_job_id=}")
+print(f"{slurm_array_task_id=}")
+print(f"{run_name=}")
+print(f"{df_or_path=}")
 
+print(f"Job started running {datetime.now():%Y-%m-%d@%H-%M}")
+print("{model_name=}")
 
-# %% submit slurm job
-subprocess.run(slurm_cmd.replace("\n    ", " "), check=True, shell=True)
-print(f"\n{submit_script = }")
+dataset_name, fold = list(product({datasets}, {folds}))[slurm_array_task_id]
+print(f"{dataset_name=}\\n{fold=}")
+
+train_wrenformer_on_matbench(
+    model_name=model_name,
+    dataset_name=dataset_name,
+    timestamp=f"{datetime.now():%Y-%m-%d@%H-%M-%S}",
+    fold=fold,
+    epochs=epochs,
+    n_attn_layers=n_attn_layers,
+    checkpoint=checkpoint,
+    learning_rate=learning_rate,
+    embedding_aggregations=embedding_aggregations,
+)
--- examples/wrenformer/mp_wbm/slurm_submit.py
+++ examples/wrenformer/mp_wbm/slurm_submit.py
@@ -1,13 +1,15 @@
 # %%
 import os
-import subprocess
 from datetime import datetime
 
+from matbench_discovery.slurm import slurm_submit_python
+
 from aviary import ROOT
+from aviary.wrenformer.train import train_wrenformer_on_df
 
 """
-Write a Python job file and sbatch it using subprocess.run() to train a Wrenformer
-ensemble of size n_folds on target_col.
+Train a Wrenformer
+ensemble of size n_folds on target_col of df_or_path.
 """
 
 __author__ = "Janosh Riebesell"
@@ -16,81 +18,60 @@ __date__ = "2022-06-13"
 
 # %%
 epochs = 300
-n_attn_layers = 3
-embedding_aggregations = ("mean",)
-optimizer = "AdamW"
-lr = 3e-4
-n_folds = 1
-df_or_path = f"{ROOT}/datasets/2022-06-09-mp+wbm.json.gz"
-target = "e_form"
-task_type = "regression"
-checkpoint = None  # None | 'local' | 'wandb'
-batch_size = 128
-run_name = f"wrenformer-robust-{epochs=}"
+target_col = "e_form"
+run_name = f"wrenformer-robust-{epochs=}-{target_col}"
+n_folds = 10
+today = f"{datetime.now():%Y-%m-%d}"
+dataset = "mp"
+log_dir = f"{os.path.dirname(__file__)}/{dataset}/{today}-{run_name}"
 
-
-log_dir = f"{os.path.dirname(__file__)}/job-logs"
-os.makedirs(log_dir, exist_ok=True)
-timestamp = f"{datetime.now():%Y-%m-%d@%H-%M-%S}"
-
-python_script = f"""import os
-from datetime import datetime
-
-from aviary.wrenformer.train import train_wrenformer_on_df
-
-print(f"Job started running {{datetime.now():%Y-%m-%d@%H-%M}}")
-print("{run_name=}")
-print("{df_or_path=}")
-
-slurm_array_task_id = int(os.environ.get("SLURM_ARRAY_TASK_ID", 0))
-
-train_wrenformer_on_df(
-    {run_name=},
-    {target=},
-    {df_or_path=},
-    {timestamp=},
-    test_size=0.05,
-    # folds=(n_folds, slurm_array_task_id),
-    {epochs=},
-    {n_attn_layers=},
-    {checkpoint=},
-    {optimizer=},
-    learning_rate={lr},
-    {embedding_aggregations=},
-    {batch_size=},
-    wandb_path="aviary/mp-wbm",
-    run_params=dict(data_path={df_or_path}),
+slurm_submit_python(
+    job_name=run_name,
+    partition="ampere",
+    account="LEE-SL3-GPU",
+    time="8:0:0",
+    array=f"1-{n_folds}",
+    log_dir=log_dir,
+    slurm_flags=("--nodes 1", "--gpus-per-node 1"),
+    # prepend into sbatch script to source module command and load default env
+    # for Ampere GPU partition before actual job command
+    pre_cmd=". /etc/profile.d/modules.sh; module load rhel8/default-amp;",
 )
-"""
-
-
-submit_script = f"{log_dir}/{timestamp}-{run_name}.py"
-
-# prepend into sbatch script to source module command and load default env
-# for Ampere GPU partition before actual job command
-slurm_setup = ". /etc/profile.d/modules.sh; module load rhel8/default-amp;"
 
 
 # %%
-slurm_cmd = f"""sbatch
-    --partition ampere
-    --account LEE-SL3-GPU
-    --time 8:0:0
-    --nodes 1
-    --gpus-per-node 1
-    --chdir {log_dir}
-    --array 0-{n_folds - 1}
-    --out {timestamp}-{run_name}-%A-%a.log
-    --job-name {run_name}
-    --wrap '{slurm_setup} python {submit_script}'
-"""
+n_attn_layers = 3
+embedding_aggregations = ("mean",)
+optimizer = "AdamW"
+learning_rate = 3e-4
+df_or_path = f"{ROOT}/datasets/2022-06-09-mp+wbm.json.gz"
+checkpoint = "wandb"  # None | 'local' | 'wandb'
+batch_size = 128
+swa_start = None
 
-header = f'"""\nSlurm submission command:\n{slurm_cmd}"""'
+slurm_job_id = os.environ["SLURM_JOB_ID"]
+slurm_array_task_id = int(os.environ.get("SLURM_ARRAY_TASK_ID", 0))
 
-with open(submit_script, "w") as file:
-    file.write(f"{header}\n\n{python_script}")
+print(f"Job started running {datetime.now():%Y-%m-%d@%H-%M}")
+print(f"{slurm_job_id=}")
+print(f"{slurm_array_task_id=}")
+print(f"{run_name=}")
+print(f"{df_or_path=}")
 
-
-# %% submit slurm job
-subprocess.run(slurm_cmd.replace("\n    ", " "), check=True, shell=True)
-print(f"\n{submit_script = }")
+train_wrenformer_on_df(
+    run_name=run_name,
+    target_col=target_col,
+    df_or_path=df_or_path,
+    timestamp=f"{datetime.now():%Y-%m-%d@%H-%M-%S}",
+    test_size=0.05,
+    # folds=(n_folds, slurm_array_task_id),
+    epochs=epochs,
+    n_attn_layers=n_attn_layers,
+    checkpoint=checkpoint,
+    optimizer=optimizer,
+    learning_rate=learning_rate,
+    embedding_aggregations=embedding_aggregations,
+    batch_size=batch_size,
+    swa_start=swa_start,
+    wandb_path="aviary/mp-wbm",
+)
