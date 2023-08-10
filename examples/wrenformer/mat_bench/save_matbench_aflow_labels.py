ae0c2fd3c0cc32a90fcbfc5682499005873395d7
Janosh Riebesell <janosh.riebesell@gmail.com>
2023-08-09 18:50:53 -0700

    v0.1.1

--- examples/wrenformer/mat_bench/save_matbench_aflow_labels.py
+++ examples/wrenformer/mat_bench/save_matbench_aflow_labels.py
@@ -15,21 +15,21 @@ to disk in the datasets/ folder as Bzip2-compressed JSON files.
 """
 
 
-mbbm = MatbenchBenchmark()
+benchmark = MatbenchBenchmark()
 
-for idx, task in enumerate(mbbm.tasks, 1):
-    print(f"\n\n{idx}/{len(mbbm.tasks)}")
+for idx, task in enumerate(benchmark.tasks, 1):
+    print(f"\n\n{idx}/{len(benchmark.tasks)}")
     task.load()
     df: pd.DataFrame = task.df
 
     if "structure" in df:
-        df["composition"] = [x.formula for x in df.structure]
+        df["composition"] = [struct.formula for struct in df.structure]
         df["wyckoff"] = [
-            get_aflow_label_from_spglib(x)
-            for x in tqdm(df.structure, desc="Getting Aflow Wyckoff labels")
+            get_aflow_label_from_spglib(struct)
+            for struct in tqdm(df.structure, desc="Getting Aflow Wyckoff labels")
         ]
     elif "composition" in df:
-        df["composition"] = [x.formula for x in df.composition]
+        df["composition"] = [comp.formula for comp in df.composition]
     else:
         raise ValueError("No structure or composition column found")
 
