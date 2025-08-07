460a8e958bfd4079edf43507d37be4a6af5a30bf
Rhys Goodall <rhys.goodall@outlook.com>
2025-08-06 22:30:53 -0400

    Add publish on release action can change to uv buld (#106)

--- aviary/__init__.py
+++ aviary/__init__.py
@@ -1,6 +1,6 @@
 import importlib.metadata
 from os.path import abspath, dirname
 
-__version__ = importlib.metadata.version("aviary")
+__version__ = importlib.metadata.version("aviary-models")
 PKG_DIR = dirname(abspath(__file__))
 ROOT = dirname(PKG_DIR)
